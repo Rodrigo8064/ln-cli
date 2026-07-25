dd863a36e3c819c06e39ba1879307d43c05d3853
rodrigo8064 <rodrigooliveiradel@gmail.com>
2026-07-24 21:26:09 -0300

    add: suport to docker and postgres

---
+++ ln_cli/commands/docker.py
@@ -0,0 +1,38 @@
+import subprocess
+from typing import Annotated
+
+from rich.console import Console
+from typer import Argument, Typer
+
+app = Typer()
+
+console = Console()
+
+
+@app.command()
+def postgres(
+    tag: Annotated[
+        str, Argument(help='Passa uma TAG para o postgres')
+    ] = 'latest',
+):
+    postgres_version = f'postgres{tag}'
+    db = subprocess.run(
+        [
+            'docker',
+            'run',
+            '-d',
+            '--name app_database',
+            '-e POSTGRES_USER=app_user',
+            '-e POSTGRES_DB=app_db',
+            '-e POSTGRES_PASSWORD=app_password',
+            '-v pgdata:/var/lib/postgresql/',
+            '-p 5432:5432',
+            postgres_version,
+        ],
+        check=True,
+    )
+    if db.returncode != 0:
+        console.print('[red]Erro ao iniciar o postgres[/]')
+        return
+
+    console.print('[green]DB Criado![/]')
